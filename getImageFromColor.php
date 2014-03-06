e908fabc4224d1909e57e65d3505705d21517430
Sam Brenner <samjbrenner@gmail.com>
2014-03-06 17:03:40 -0500

    returning image as json

--- getImageFromColor.php
+++ getImageFromColor.php
@@ -5,8 +5,6 @@
   $searchResults = file_get_contents('https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.search.objects&access_token=' . $accessToken . '&color=' . $color . '&has_images=true');
   $searchResults = json_decode($searchResults)->objects;
 
-  //pick random object
-  echo '<img src="'. $searchResults[array_rand($searchResults)]->images[0]->b->url . '" />';
-
-
+  header('Content-Type: application/json');
+  echo '{"url":"'. $searchResults[array_rand($searchResults)]->images[0]->b->url . '"}';
 ?>
