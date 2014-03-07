f588c81b5f5d7ee0884a5206f04a74a9fedf0c53
Sam Brenner <samjbrenner@gmail.com>
2014-03-07 17:47:18 -0500

    getting artists name from image

--- getImageFromColor.php
+++ getImageFromColor.php
@@ -5,6 +5,61 @@
   $searchResults = file_get_contents('https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.search.objects&access_token=' . $accessToken . '&color=' . $color . '&has_images=true');
   $searchResults = json_decode($searchResults)->objects;
 
+  $object = $searchResults[array_rand($searchResults)];
+
   header('Content-Type: application/json');
-  echo '{"url":"'. $searchResults[array_rand($searchResults)]->images[0]->b->url . '"}';
+  echo '{';
+    echo '"url":"'. $object->images[0]->b->url . '",';
+    echo '"credits":"' . getCreativePeople($object->participants) . '"';
+  echo '}';
+
+  function getCreativePeople($people) {
+    $primaryPeople = array_filter($people, function($person) {
+      $returnVal = false;
+
+      switch($person->role_name) {
+        case 'Designer':
+        case 'Artist':
+        case 'Maker':
+        case 'Embroiderer':
+        case 'Engraver':
+        case 'Graphic Designer':
+        case 'Etcher':
+        case 'Photographer':
+        case 'Creator':
+        case 'Model maker':
+        case 'Modeler':
+        case 'Textile Artist':
+        case 'Ceramist':
+        case 'Fabricator':
+        case 'Jeweler':
+        case 'Cartoonist':
+        case 'Glass Worker':
+        case 'Architect':
+        case 'Landscape Architect':
+        case 'Art Director':
+        case 'Design Director':
+          $returnVal = true;
+          break;
+      }
+
+      return $returnVal;
+    });
+
+    $primaryPeopleString = '';
+    $count = 0;
+
+    if($primaryPeople) {
+      foreach($primaryPeople as $primaryPerson) { 
+        if($count > 0) $primaryPeopleString .= ', ';
+        $primaryPeopleString .= ($primaryPerson->person_name);
+
+        $count++;
+      }
+    } else {
+      $primaryPeopleString = 'The Cooper Hewitt Collection';
+    }
+
+    return $primaryPeopleString;
+  }
 ?>
